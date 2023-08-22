fee9e4b20036bdd12113e7ed3d629a7d284cb4b1
gabcqw <134643615+gabcqw@users.noreply.github.com>
2023-08-22 21:55:17 +0100

    Add files via upload

--- extendedreader.py
+++ extendedreader.py
@@ -55,76 +55,76 @@ def write(output_file_name, changed_content):
             return pickle.dump(changed_content, f)
 
 
-if __name__ == "_main_":
 
     # Add validation of input
 
     # Get variables
-    input_file_name = sys.argv[1]
-    output_file_name = sys.argv[2]
-    changes = sys.argv[3:]
+input_file_name = sys.argv[1]
+output_file_name = sys.argv[2]
+changes = sys.argv[3:]
     
-    class CsvHandler:
-        def read(self, file_name):
-            data = []
+class CsvHandler:
+    def read(self, file_name):
+        data = []
 
-            with open(file_name, "r") as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    data.append(row)
+        with open(file_name, "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                data.append(row)
 
             return data
 
-        def write(self, file_name, content):
-            with open(file_name, "w", endline="") as f:
-                writer = csv.writer(f)
-                writer.writerows(content)
+    def write(self, file_name, content):
+        #with open(file_name, "w") as f:?
+        with open(file_name, "w", endline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(content)
 
-    class PklHandler:
-        def read(self, file_name):
-            with open(file_name, "rb") as f:
-             return pickle.load(f)
-        def write(self, content, file_name):
-            with open(file_name, "wb") as f:
-                return pickle.dump(content, f)
+class PklHandler:
+    def read(self, file_name):
+        with open(file_name, "rb") as f:
+            return pickle.load(f)
+    def write(self, content, file_name):
+        with open(file_name, "wb") as f:
+            return pickle.dump(content, f)
             
-    class JsonHandler:
-        def read(self, file_name):
-            with open(file_name, "rb") as f:
-                return json.load(f)
-        def write(self, content, file_name):
-            with open(file_name, "wb") as f:
-                return json.dump(content, f)
+class JsonHandler:
+    def read(self, file_name):
+        with open(file_name, "rb") as f:
+            return json.load(f)
+    def write(self, content, file_name):
+        with open(file_name, "wb") as f:
+            return json.dump(content, f)
 
 
 
-    def select_handler(file_name):
-        if file_name.endswith(".csv"):
-            return CsvHandler()
-        elif file_name.endswith(".json"):
-            return JsonHandler()
-        elif file_name.endswith(".pickle"):
-           return PklHandler()
-        else:
-            raise Exception(f"File type not supported: {input_file_name}. You need to try enter the other file name.")
+def select_handler(file_name):
+    if file_name.endswith(".csv"):
+        return CsvHandler()
+    elif file_name.endswith(".json"):
+        return JsonHandler()
+    elif file_name.endswith(".pickle"):
+        return PklHandler()
+    else:
+        raise Exception(f"File type not supported: {input_file_name}. You need to try enter the other file name.")
 
-    if not os.path.exists(input_file_name):
-        all_file = os.path.spilt(input_file_name)[0]
-        if not all_file:
-            all_file = os.getcwd()
-        other_files = [
-            f for f in os.listdir(all_file)
-            if not os.path.isdir(f) and (".csv" in f or ".json" in f or ". pkl" in f)
+if not os.path.exists(input_file_name):
+    all_file = os.path.spilt(input_file_name)[0]
+    if not all_file:
+        all_file = os.getcwd()
+    other_files = [
+        f for f in os.listdir(all_file)
+        if not os.path.isdir(f) and (".csv" in f or ".json" in f or ". pkl" in f)
         ]
 
 
     
-    file = select_handler(input_file_name)
-    data1 = file.read(input_file_name)
-    data2 = modify_content(data1, changes)
-    file.write(data2, output_file_name)
-    print(data1)
-    print(data2)
+file = select_handler(input_file_name)
+data1 = file.read(input_file_name)
+data2 = modify_content(data1, changes)
+file.write(data2, output_file_name)
+print(data1)
+print(data2)
  
 
 
